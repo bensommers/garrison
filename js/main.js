401a1c6e90329acc066240da6cf797ee1014a0f5
BenFromCommand <sommersbhms@gmail.com>
2017-10-14 15:58:53 -0400

    move static markup for work examples into React components

--- js/main.js
+++ js/main.js
@@ -2,4 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ExampleWork from './example-work.js';
 
-ReactDOM.render(<ExampleWork />, document.getElementByID('example-work'));
+const myWork = [
+  {
+    'title': "HOME",
+    'image': {
+      'desc': "house",
+      'src': "images/example1.png",
+      'comment': "",
+    }
+  },
+  {
+    'title': "Pets",
+    'image': {
+      'desc': "Pets Description",
+      'src': "images/example2.png",
+      'comment': "",
+    }
+  },
+  {
+    'title': "Automobiles",
+    'image': {
+      'desc': "cars and shit",
+      'src': "images/example3.png",
+      'comment': "",
+    }
+  }
+]
+
+ReactDOM.render(<ExampleWork work={myWork} />, document.getElementById('example-work'));
