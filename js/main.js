bc54ff182e438723ad51abfa4b2e397b5b03d1c2
BenFromCommand <sommersbhms@gmail.com>
2017-10-15 16:49:29 -0400

    showing Em

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ const myWork = [
   {
     'title': "HOME",
     'href': "https://example.com",
-    'desc':"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    'desc':"Protect your house with state of the art GarrisonHomeDefense, don't be a doorknob!",
     'image': {
       'desc': "example img",
       'src': "images/example1.png",
@@ -16,7 +16,7 @@ const myWork = [
   {
     'title': "Pets",
     'href': "https://example.com",
-    'desc':"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    'desc':"Protect your pets with the power of GarrisonPaws, order right meow!",
     'image': {
       'desc': "Pets Description",
       'src': "images/example2.png",
@@ -26,7 +26,7 @@ const myWork = [
   {
     'title': "Automobiles",
     'href': "https://example.com",
-    'desc':"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    'desc':"Protect your autos with GarrisonAutoShield, it's wheely great!",
     'image': {
       'desc': "cars and shit",
       'src': "images/example3.png",
